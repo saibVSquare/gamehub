f8c39af7b4cd0ad0c0c6ee1348e6c6ff07af15af
Saib Ali <saib.ali@viralsquare.org>
2023-10-04 18:52:04 +0500

    update some file for tsx extention

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
+// @ts-ignore
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// @ts-ignore
+import App from './App.tsx';
+import {ChakraProvider} from "@chakra-ui/react";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider>
+            <App/>
+        </ChakraProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
